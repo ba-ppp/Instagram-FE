ffde66b2c95de7c49543b19d89a8f77372fa3443
ba-p <phuongtr4916@gmail.com>
2020-06-04 09:42:03 +0700

    Set token

--- src/components/JS/Login.js
+++ src/components/JS/Login.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
- const LoginA = () => {
-    return(
-        <div>hi</div>
-    )
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      token: ""
+    };
+    this.postUsername = this.postUsername.bind(this);
+    this.postPassword = this.postPassword.bind(this);
+    this.postLogin = this.postLogin.bind(this);
+  }
+  postUsername(event) {
+    this.setState({ username: event.target.value });
+  }
+  postPassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  postLogin(event) {
+    const username = this.state.username;
+    const password = this.state.password;
+    const data = {
+      username: username,
+      password: password
+    };
+    console.log(data);
+    axios
+      .post("https://prs8c.sse.codesandbox.io/login", data)
+      .then(res => {
+        this.setState({ token: res.data.token });
+        console.log(this.state.token);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <label>username </label>
+          <input name="username" onChange={this.postUsername} />
+        </div>
+        <div>
+          <label>password</label>
+          <input name="password" type="password" onChange={this.postPassword} />
+        </div>
+        <button type="submit" onClick={this.postLogin}>
+          Send
+        </button>
+      </div>
+    );
+  }
 }
-export default LoginA;
